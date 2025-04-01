a1b8f4e99feb6b5e979fd42dc276c0190e8ad90f
Janos Kishazi <janos.kishazi@routepatch.com>
2025-04-01 21:31:09 +0200

    Add multi-objective weight check.

--- multi_objective_regression/utils/training_parameter_utility.py
+++ multi_objective_regression/utils/training_parameter_utility.py
@@ -1,3 +1,4 @@
+import math
 from abc import ABC
 
 from dto.training_parameters import TrainingParameters
@@ -15,5 +16,13 @@ class TrainingParameterUtility(ABC):
         training_parameters: TrainingParameters = TrainingParameters.from_json(
             training_parameters_file_content
         )
+
+        sum_of_weights: float = 0.0
+        for weight in training_parameters.multi_objective_function_weights.values():
+            sum_of_weights += weight
+        if not math.isclose(sum_of_weights, 1.0):
+            raise ValueError(
+                f"The sum of multi-objective weights is not 1.0, instead of {sum_of_weights}."
+            )
 
         return training_parameters
