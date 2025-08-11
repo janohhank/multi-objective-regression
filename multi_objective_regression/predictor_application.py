837f21b3abeba912f23b119d79bb06fa030e9816
Janos Kishazi <janos.kishazi@routepatch.com>
2025-08-11 12:31:56 +0200

    Add threshold propagation option for predictor application.

--- multi_objective_regression/predictor_application.py
+++ multi_objective_regression/predictor_application.py
@@ -4,6 +4,8 @@ import os.path
 
 import joblib
 import pandas
+import seaborn
+from matplotlib import pyplot as plt
 from pandas import DataFrame
 from sklearn.metrics import (
     accuracy_score,
@@ -12,6 +14,7 @@ from sklearn.metrics import (
     f1_score,
     roc_auc_score,
     average_precision_score,
+    confusion_matrix,
 )
 
 from dto.training_result import TrainingResult
@@ -20,10 +23,44 @@ from dto.training_result import TrainingResult
 class MultiObjectivePredictorApplication:
     __MODEL_PATH: str = None
     __DATASET_PATH: str = None
+    __THRESHOLD: float = None
 
-    def __init__(self, model_path: str, dataset_path: str) -> None:
+    def __init__(
+        self, model_path: str, dataset_path: str, threshold: float = 0.5
+    ) -> None:
         self.__MODEL_PATH = model_path
         self.__DATASET_PATH = dataset_path
+        self.__THRESHOLD = threshold
+
+    def plot_confusion_matrix(
+        self,
+        y_test,
+        y_pred,
+    ):
+        cm = confusion_matrix(y_test, y_pred)
+        labels = ["Negative", "Positive"]  # Adjust as needed
+
+        plt.figure(figsize=(5, 4))
+        seaborn.heatmap(
+            cm,
+            annot=True,
+            fmt="d",
+            cmap="Blues",
+            xticklabels=labels,
+            yticklabels=labels,
+        )
+        plt.xlabel("Predicted Label")
+        plt.ylabel("True Label")
+        plt.title("Confusion Matrix")
+        plt.tight_layout()
+
+        plt.savefig(
+            os.path.join("confusion_matrix.pdf"),
+            format="pdf",
+            dpi=300,
+            bbox_inches="tight",
+        )
+        plt.clf()
 
     def start_prediction(self) -> None:
         model = joblib.load(os.path.join(self.__MODEL_PATH, "model.pkl"))
@@ -47,7 +84,7 @@ class MultiObjectivePredictorApplication:
         ].copy()
 
         y_probs = model.predict_proba(x_test_scaled)[:, 1]
-        y_pred = model.predict(x_test_scaled)
+        y_pred = (y_probs >= self.__THRESHOLD).astype(int)
 
         accuracy: float = accuracy_score(y_test, y_pred)
         precision: float = precision_score(y_test, y_pred)
@@ -64,6 +101,8 @@ class MultiObjectivePredictorApplication:
         print(f"ROC-AUC: {roc_auc}")
         print(f"PR-AUC: {pr_auc}")
         print(f"Gini score: {gini_score}")
+
+        self.plot_confusion_matrix(y_test, y_pred)
 
 
 def main():
@@ -82,11 +121,19 @@ def main():
         required=True,
         help="Dataset path.",
     )
+    parser.add_argument(
+        "--threshold",
+        type=float,
+        required=False,
+        help="Threshold.",
+    )
 
     args = parser.parse_args()
 
     application: MultiObjectivePredictorApplication = (
-        MultiObjectivePredictorApplication(args.model_path, args.dataset_path)
+        MultiObjectivePredictorApplication(
+            args.model_path, args.dataset_path, args.threshold
+        )
     )
     application.start_prediction()
 
